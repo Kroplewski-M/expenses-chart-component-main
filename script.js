f7906b4154de90e85ee80a1a4da6ec0c1060e53c
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-06-28 14:40:27 +0100

    finished mobile view

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
 let graphInfo;
-
+let mon;
+let GI;
 //GET JSON DATA AND STORE IT IN GRAPHINFO
 fetch("data.json")
   .then((Response) => Response.json())
   .then((data) => {
     graphInfo = data;
-    console.log(graphInfo[4].amount);
+    console.log(graphInfo[0].amount);
+    mon = JSON.stringify(JSON.parse(graphInfo[0].amount));
+    drawGraph();
+  })
+  .catch((error) => {
+    console.log(error);
   });
+
+//DRAW GRAPH USING CHARTJS
+function drawGraph() {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      datasets: [
+        {
+          label: "",
+          data: [
+            graphInfo[0].amount,
+            graphInfo[1].amount,
+            graphInfo[2].amount,
+            graphInfo[3].amount,
+            graphInfo[4].amount,
+            graphInfo[5].amount,
+            graphInfo[6].amount,
+          ],
+          backgroundColor: [
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(64,224,208, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderColor: [
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(64,224,208, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          display: false,
+        },
+        grid: {
+          display: false,
+        },
+      },
+    },
+  });
+}
